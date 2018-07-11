cbaddb67f9de4e8d2c2f3f207fae4592de5a0ddb
Matt Johnson <matt@compensationconnections.com>
2018-07-10 22:37:12 -0700

    Sample app from here: https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-2.1&tabs=visual-studio

---
+++ ContosoUniversity/Models/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class CourseViewModel
+{
+    public int CourseID { get; set; }
+    public string Title { get; set; }
+    public int Credits { get; set; }
+
+    [Display(Name = "Department Name")]
+    public string DepartmentName { get; set; }
+}
